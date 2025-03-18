965813fc5fb9b6cdb5e8d3a897a366bc95f664d8
Jess Long <jesslong@Mac.lan>
2025-03-17 22:44:01 -0400

    cards are now flexible

--- scripts/index.js
+++ scripts/index.js
@@ -37,6 +37,25 @@ const editModalDescriptionInput=editModal.querySelector(
   "#profile-description-input"
 );
 
+const cardTemplate=document.querySelector("#card-template");
+const cardsList=document.querySelector(".cards__list");
+
+function getCardElement(data) {
+const cardElement = cardTemplate.content
+  .querySelector(".card")
+  .cloneNode(true);
+
+const cardNameEl=cardElement.querySelector(".card__title");
+const cardImageEl=cardElement.querySelector(".card__image");
+
+cardNameEl.textContent=data.name;
+cardImageEl.src=data.link;
+cardImageEl.alt=data.name;
+// assign values to image src and alt
+
+return cardElement;
+}
+
 function openModal() {
   editModalNameInput.value=profileName.textContent;
   editModalDescriptionInput.value=profileDescription.textContent;
@@ -57,3 +76,8 @@ function handleEditFormSubmit(evt) {
 profileEditBtn.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i=0; i<initialCards.length; i++) {
+  const cardElement=getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
